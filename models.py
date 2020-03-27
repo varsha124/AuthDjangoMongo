9551317ba3477a19d327ef247ede31e375ae68d7
varsha124 <55065229+varsha124@users.noreply.github.com>
2020-03-27 18:46:53 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=3)
+    birth_date = models.DateField(blank=True, null=True)
